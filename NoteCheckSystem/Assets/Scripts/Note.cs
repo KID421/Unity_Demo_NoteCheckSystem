e24e8ba91e34e85c7b5ca6ae1e1c7b739d59c9d7
KID <50666207+KID421@users.noreply.github.com>
2020-10-26 20:00:42 +0800

    完成

---
+++ NoteCheckSystem/Assets/Scripts/Note.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    public float speed;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(-speed * Time.deltaTime, 0, 0);
+    }
+}
---
+++ NoteCheckSystem/Assets/Scripts/NoteCheckSystem.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NoteCheckSystem : MonoBehaviour
+{
+    public float rangePerfect;
+    public float rangeGood;
+    public float rangeBad;
+
+    public Transform target = null;
+
+    public Text textTip;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "音符" && target == null) target = collision.transform;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "音符" && target != null) target = null;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "音符" && target == null) target = collision.transform;
+    }
+
+    private void Update()
+    {
+        if (target != null && Input.GetKeyDown(KeyCode.Mouse0)) CheckDistance();
+    }
+
+    /// <summary>
+    /// 檢查距離
+    /// </summary>
+    private void CheckDistance()
+    {
+        float dis = Vector3.Distance(transform.position, target.position);
+
+        if (dis <=  rangePerfect)
+        {
+            print("PERFECT!!!");
+            Destroy(target.gameObject);
+            textTip.text = "Perfect!!!";
+        }
+        else if (dis <= rangeGood)
+        {
+            print("GOOD!!!");
+            Destroy(target.gameObject);
+            textTip.text = "Good!!!";
+        }
+        else if (dis <= rangeBad)
+        {
+            print("BAD!!!");
+            Destroy(target.gameObject);
+            textTip.text = "Bad!!!";
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, rangePerfect);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, rangeGood);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, rangeBad);
+    }
+}
